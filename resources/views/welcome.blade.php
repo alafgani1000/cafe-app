a903c268187ddbc13c56b81e707ed3f1f3819875
alafgani1000 <irhabyaganal625@gmail.com>
2022-01-16 08:44:55 +0700

    change name role

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,7 +25,7 @@
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        @role('pramuniaga')
+                        @role('pramusaji')
                             <a href="{{ route('makanan') }}" class="text-sm text-gray-700 btn btn-primary">Home</a>
                         @endrole
                         @role('admin')
